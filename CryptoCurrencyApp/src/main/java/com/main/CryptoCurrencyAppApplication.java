634f909fb0cc098541b6c650c96bc5b1f83a5654
Artur Filipini <filipiniartur@gmail.com>
2022-11-17 17:36:57 -0300

    Changed Gateway Zuluu for Spring Cloud Gateway because it was deprecated

--- CryptoCurrencyApp/src/main/java/com/main/CryptoCurrencyAppApplication.java
+++ CryptoCurrencyApp/src/main/java/com/main/CryptoCurrencyAppApplication.java
@@ -2,10 +2,10 @@ package com.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class CryptoCurrencyAppApplication {
 
 	public static void main(String[] args) {
--- gateway/src/main/java/com/CryptoCurrencyApp/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/CryptoCurrencyApp/gateway/GatewayApplication.java
@@ -2,12 +2,10 @@ package com.CryptoCurrencyApp.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableZuulProxy
-@EnableEurekaServer
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
