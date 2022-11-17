e84b5d24e31e7b82686843ed9a095bab65a4c8b1
Artur Filipini <filipiniartur@gmail.com>
2022-11-16 21:36:52 -0300

    Added Service Eureka and GateWay Zuluu

---
+++ CryptoCurrencyApp/src/main/java/com/main/Controller/MainController.java
@@ -0,0 +1,14 @@
+package com.main.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class MainController {
+    @GetMapping("hello")
+    public String hello(){
+        return "hello world";
+    }
+}
---
+++ gateway/src/main/java/com/CryptoCurrencyApp/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.CryptoCurrencyApp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaServer
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
